d9eab1d296bf32b7268bdc89d333ddf80b65a415
alshevskiy <nikita.alshevskiy@gmail.com>
2022-10-17 19:36:32 +0300

    1.7. Конструктор

---
+++ src/main/java/ru/job4j/tracker/oop/Error.java
@@ -0,0 +1,34 @@
+package ru.job4j.tracker.oop;
+
+public class Error {
+
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error error0 = new Error(false, 0, "Нет ошибки");
+        Error error1 = new Error(true, 1, "Абонент не существует");
+        Error error6 = new Error(true, 6, "Абонент не в сети");
+        error.printInfo();
+        error0.printInfo();
+        error1.printInfo();
+        error6.printInfo();
+    }
+}
