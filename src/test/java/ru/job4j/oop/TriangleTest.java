ead11ac47d9835ee532a102283eee8c14d11619a
alshevskiy <nikita.alshevskiy@gmail.com>
2022-10-20 15:34:48 +0300

    2.2. Рефакторинг - Площадь треугольника.

--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -19,11 +19,33 @@ public class TriangleTest {
     @Test
     public void when00and00and00ThenMinus1() {
         Point a = new Point(0, 0);
-        Point b = new Point(0, 0);
+        Point b = new Point(2, 3);
         Point c = new Point(0, 0);
         Triangle triangle = new Triangle(a, b, c);
         double rsl = triangle.area();
         double expected = -1;
+        assertThat(rsl).isCloseTo(expected, offset(0.001));
+    }
+
+    @Test
+    public void when12and34and45ThenMinus1() {
+        Point a = new Point(1, 2);
+        Point b = new Point(3, 4);
+        Point c = new Point(5, 6);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        double expected = -1;
+        assertThat(rsl).isCloseTo(expected, offset(0.001));
+    }
+
+    @Test
+    public void when11and22and33Then4() {
+        Point a = new Point(1, 2);
+        Point b = new Point(-1, -2);
+        Point c = new Point(5, 6);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        double expected = 4;
         assertThat(rsl).isCloseTo(expected, offset(0.001));
     }
 }
