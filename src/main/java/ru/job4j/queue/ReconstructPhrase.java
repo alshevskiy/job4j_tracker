8c48ae8cb61ebb5ba3e6d9d571bf275fae8f8c30
Nikita Alshevskiy <nikita.alshevskiy@gmail.com>
2023-09-17 16:38:38 +0300

    2. Интерфейс Deque [#504943 #445231]

--- src/main/java/ru/job4j/queue/ReconstructPhrase.java
+++ src/main/java/ru/job4j/queue/ReconstructPhrase.java
@@ -13,26 +13,21 @@ public class ReconstructPhrase {
 
     private String getDescendingElements() {
         StringBuilder result = new StringBuilder();
-        result.ensureCapacity(descendingElements.size());
-
-        for (int i = 0; i < descendingElements.size(); i++) {
+        while (!descendingElements.isEmpty()) {
             result.append(descendingElements.pollLast());
         }
-
         return result.toString();
     }
 
     private String getEvenElements() {
         StringBuilder result = new StringBuilder();
         int counter = 0;
-
         for (Character ch : evenElements) {
             if ((counter % 2) == 0) {
                 result.append(ch);
             }
             counter++;
         }
-
         return result.toString();
     }
 
